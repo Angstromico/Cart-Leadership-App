14b306423a5c3f4f6a9fca2f75bfad708b5e4b4a
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-23 15:39:47 -0500

    feat: Find carts by id

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('carts')
 export class CartsController {
+  private carts = [
+    { id: 1, items: ['Bear', 'Car'] },
+    { id: 2, items: ['Bike', 'Scooter'] },
+  ];
+
   @Get()
   getCarts() {
-    return { message: 'List of carts' };
+    return this.carts;
+  }
+
+  @Get(':id')
+  getCartById(@Param('id') id: string) {
+    console.log('Fetching cart with id:', id);
+    const numberId = parseInt(id, 10);
+    if (isNaN(numberId)) {
+      return { message: 'Invalid cart ID' };
+    }
+    const cart = this.carts.find((cart) => cart.id === numberId);
+
+    return cart ? cart : { message: 'Cart not found' };
   }
 }
