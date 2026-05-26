0fb15cbf8063c5faee51d8738c1a8bc0e2df9e52
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-26 06:02:10 -0400

    feat: use decorator on DTO create cart

--- src/carts/dto/create-cart.dto.ts
+++ src/carts/dto/create-cart.dto.ts
@@ -1,4 +1,10 @@
+import { IsArray, IsString } from 'class-validator';
+
 export class CreateCartDTO {
+  @IsString()
   readonly id: string;
+
+  @IsArray()
+  @IsString({ each: true })
   readonly items: string[];
 }
